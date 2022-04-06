5287d0e00a3fcec7afc5e66fae0e7303a35741fe
brayanbertan <brayanbertan@gmail.com>
2022-04-06 16:21:33 -0300

    Text editor

--- lib/app/modules/opinioes/widgets/postar_opiniao/page_postar_opiniao.dart
+++ lib/app/modules/opinioes/widgets/postar_opiniao/page_postar_opiniao.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 
 class PagePostarOpiniao extends StatelessWidget {
   PagePostarOpiniao({Key? key}) : super(key: key);
-
+  QuillController _controller_editor = QuillController.basic();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Nova Postagem'),
-      ),
+      appBar: AppBar(),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(10),
           child: Column(
-            children: [],
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_controller_editor.document.toDelta());
+                },
+                child: const Icon(Icons.add),
+              ),
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                  child: QuillToolbar.basic(controller: _controller_editor)),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.4,
+                child: QuillEditor.basic(
+                  controller: _controller_editor,
+                  readOnly: false,
+                ),
+              ),
+            ],
           ),
         ),
       ),
